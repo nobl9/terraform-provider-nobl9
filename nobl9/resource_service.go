61e99a0e8735f743a923c20f0b9036b5749d3af2
RemiLis <remi@nobl9.com>
2021-09-22 13:03:38 +0200

    Add Service

--- nobl9/resource_service.go
+++ nobl9/resource_service.go
@@ -11,155 +11,134 @@ import (
 func resourceService() *schema.Resource {
 	return &schema.Resource{
 		Schema: map[string]*schema.Schema{
-			"api_version": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "API version",
-			},
+			"name":         schemaName(),
+			"display_name": schemaDisplayName(),
+			"project":      schemaProject(),
+			"description":  schemaDescription(),
 
-			"kind": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Kind of object",
-			},
-
-			"manifest_src": {
-				Type:        schema.TypeString,
+			"service_spec": {
+				Type:        schema.TypeSet,
 				Optional:    true,
-				Description: "",
-			},
-
-			"metadata": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: "",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"display_name": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Description: "",
-						},
-
-						"labels": {
-							Type:        schema.TypeSet,
-							Optional:    true,
-							Description: "",
-							Elem:        &schema.Schema{Type: schema.TypeString},
-						},
-
-						"name": {
-							Type:        schema.TypeString,
-							Required:    true,
-							ForceNew:    true,
-							Description: "",
-						},
-
-						"project": {
-							Type:        schema.TypeString,
-							Optional:    true,
-							Description: "",
-						},
-					},
-				},
-			},
-
-			"organization": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "",
-			},
-
-			"spec": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: "",
+				Description: "Specifications of the service",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"description": {
 							Type:        schema.TypeString,
 							Optional:    true,
-							Description: "",
+							Description: "Description of the service",
 						},
 					},
 				},
 			},
 
 			"status": {
-				Type:        schema.TypeList,
-				Optional:    true,
-				Description: "",
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"slo_count": {
-							Type:        schema.TypeInt,
-							Optional:    true,
-							Description: "",
-						},
-					},
-				},
+				Type:        schema.TypeMap,
+				Computed:    true,
+				Description: "Status of created service.",
 			},
 		},
-		CreateContext: resourceServiceCreate,
-		UpdateContext: resourceServiceUpdate,
-		DeleteContext: resourceServiceDelete,
+		CreateContext: resourceServiceApply,
 		ReadContext:   resourceServiceRead,
-		Description:   "* [HTTP API](https://api-docs.app.nobl9.com/)",
+		UpdateContext: resourceServiceApply,
+		DeleteContext: resourceServiceDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Description: "* [Service configuration documentation](https://nobl9.github.io/techdocs_YAML_Guide/#service)",
 	}
 }
 
 func marshalService(d *schema.ResourceData) *n9api.Service {
+
 	return &n9api.Service{
 		ObjectHeader: n9api.ObjectHeader{
-			APIVersion: d.Get("api_version").(string),
-			Kind:       d.Get("kind").(string),
-			MetadataHolder: n9api.MetadataHolder{
-				Metadata: n9api.Metadata{
-					Name: d.Get("name").(string),
-				},
-			},
+			APIVersion:     n9api.APIVersion,
+			Kind:           "Service",
+			MetadataHolder: marshalMetadata(d),
+		},
+		Spec: n9api.ServiceSpec{
+			Description: d.Get("description").(string),
 		},
 	}
 }
 
-func resourceServiceCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	c := meta.(*n9api.Client)
-
-	// Warning or errors can be collected in a slice type
+func unmarshalService(d *schema.ResourceData, objects []n9api.AnyJSONObj) diag.Diagnostic {
+	if len(objects) != 1 {
+		d.SetId("")
+		return nil
+	}
+	object := objects[0]
 	var diags diag.Diagnostics
 
-	service := marshalService(d)
-	var p n9api.Payload
-	p.AddObject(service)
-
-	err := c.ApplyObjects(p.GetObjects())
-	if err != nil {
-		return diag.Errorf("could not add service: %v", err)
+	if ds := unmarshalMetadata(object, d); ds.HasError() {
+		diags = append(diags, ds...)
 	}
 
-	//d.SetId(strconv.FormatInt(id, 10))
+	status := object["status"].(map[string]interface{})
+	err := d.Set("status", status)
+	appendError(diags, err)
+
+	spec := object["spec"].(map[string]interface{})
+
+	err = d.Set("description", spec["description"])
+	appendError(diags, err)
 
 	return diags
 }
 
-func resourceServiceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	//c := meta.(*n9api.Client)
+func resourceServiceApply(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(ProviderConfig)
+	client, ds := newClient(config, d.Get("project").(string))
+	if ds != nil {
+		return ds
+	}
 
-	//d.SetId(strconv.FormatInt(id, 10))
+	service := marshalService(d)
 
-	return nil
+	var p n9api.Payload
+	p.AddObject(service)
+
+	err := client.ApplyObjects(p.GetObjects())
+	if err != nil {
+		return diag.Errorf("could not add service: %s", err.Error())
+	}
+
+	d.SetId(service.Metadata.Name)
+
+	return resourceAgentRead(ctx, d, meta)
 }
 
-func resourceServiceUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+func resourceServiceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(ProviderConfig)
+	project := d.Get("project").(string)
+	if project == "" {
+		// project is empty when importing
+		project = config.Project
+	}
+	client, ds := newClient(config, project)
+	if ds.HasError() {
+		return ds
+	}
 
-	//d.SetId(strconv.FormatInt(id, 10))
+	objects, err := client.GetObject(n9api.ObjectService, "", d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
-	return nil
+	return unmarshalService(d, objects)
 }
 
 func resourceServiceDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(ProviderConfig)
+	client, ds := newClient(config, d.Get("project").(string))
+	if ds.HasError() {
+		return ds
+	}
 
-	//d.SetId(strconv.FormatInt(id, 10))
+	err := client.DeleteObjectsByName(n9api.ObjectService, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
 	return nil
 }
