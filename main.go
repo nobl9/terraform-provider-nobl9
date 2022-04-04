68e27e342a70289a427f9e3fcb86311c98f49fc9
ian-bartholomew <ibartholomew@gmail.com>
2022-04-04 10:51:19 -0700

    Adding debugging options

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"log"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/nobl9/terraform-provider-nobl9/nobl9"
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{
+	var debugMode bool
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{
 		ProviderFunc: nobl9.Provider,
-	})
+	}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "nobl9.com/nobl9/nobl9", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
