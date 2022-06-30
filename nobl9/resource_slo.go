4c2deaef75823f219574755db3d852aadec507d1
Jakub <jakuburghardt@gmail.com>
2022-06-30 13:36:54 +0200

    init cloudwatch

--- nobl9/resource_slo.go
+++ nobl9/resource_slo.go
@@ -273,7 +273,6 @@ func equalSlices(a, b []interface{}) bool {
 }
 
 func marshalSLO(d *schema.ResourceData) *n9api.SLO {
-
 	indicator := marshalIndicator(d)
 	return &n9api.SLO{
 		ObjectHeader: n9api.ObjectHeader{
@@ -296,7 +295,6 @@ func marshalSLO(d *schema.ResourceData) *n9api.SLO {
 }
 
 func marshalComposite(d *schema.ResourceData) *n9api.Composite {
-
 	compositeSet := d.Get("composite").(*schema.Set)
 
 	if compositeSet.Len() > 0 {
@@ -455,6 +453,7 @@ func marshalMetric(metric map[string]interface{}) *n9api.MetricSpec {
 		BigQuery:            marshalSLOBigQuery(metric["bigquery"].(*schema.Set)),
 		OpenTSDB:            marshalSLOOpenTSDB(metric["opentsdb"].(*schema.Set)),
 		GrafanaLoki:         marshalSLOGrafanaLoki(metric["grafana_loki"].(*schema.Set)),
+		CloudWatch:          marshalSLOCloudWatch(metric["cloudwatch"].(*schema.Set)),
 	}
 }
 
@@ -509,6 +508,7 @@ func marshalSLOAppDynamics(s *schema.Set) *n9api.AppDynamicsMetric {
 		MetricPath:      &metricPath,
 	}
 }
+
 func marshalSLOSplunk(s *schema.Set) *n9api.SplunkMetric {
 	if s.Len() == 0 {
 		return nil
@@ -521,6 +521,7 @@ func marshalSLOSplunk(s *schema.Set) *n9api.SplunkMetric {
 		Query: &query,
 	}
 }
+
 func marshalSLOLightstep(s *schema.Set) *n9api.LightstepMetric {
 	if s.Len() == 0 {
 		return nil
@@ -543,6 +544,7 @@ func marshalSLOLightstep(s *schema.Set) *n9api.LightstepMetric {
 		Percentile: percentile,
 	}
 }
+
 func marshalSLOSplunkObservability(s *schema.Set) *n9api.SplunkObservabilityMetric {
 	if s.Len() == 0 {
 		return nil
@@ -555,6 +557,7 @@ func marshalSLOSplunkObservability(s *schema.Set) *n9api.SplunkObservabilityMetr
 		Program: &program,
 	}
 }
+
 func marshalSLODynatrace(s *schema.Set) *n9api.DynatraceMetric {
 	if s.Len() == 0 {
 		return nil
@@ -567,6 +570,7 @@ func marshalSLODynatrace(s *schema.Set) *n9api.DynatraceMetric {
 		MetricSelector: &selector,
 	}
 }
+
 func marshalSLOThousandEyes(s *schema.Set) *n9api.ThousandEyesMetric {
 	if s.Len() == 0 {
 		return nil
@@ -579,6 +583,7 @@ func marshalSLOThousandEyes(s *schema.Set) *n9api.ThousandEyesMetric {
 		TestID: &testID,
 	}
 }
+
 func marshalSLOGraphite(s *schema.Set) *n9api.GraphiteMetric {
 	if s.Len() == 0 {
 		return nil
@@ -591,6 +596,7 @@ func marshalSLOGraphite(s *schema.Set) *n9api.GraphiteMetric {
 		MetricPath: &metricPath,
 	}
 }
+
 func marshalSLOBigQuery(s *schema.Set) *n9api.BigQueryMetric {
 	if s.Len() == 0 {
 		return nil
@@ -604,6 +610,7 @@ func marshalSLOBigQuery(s *schema.Set) *n9api.BigQueryMetric {
 		Location:  metric["location"].(string),
 	}
 }
+
 func marshalSLOOpenTSDB(s *schema.Set) *n9api.OpenTSDBMetric {
 	if s.Len() == 0 {
 		return nil
@@ -627,6 +634,65 @@ func marshalSLOGrafanaLoki(s *schema.Set) *n9api.GrafanaLokiMetric {
 	logql := metric["logql"].(string)
 	return &n9api.GrafanaLokiMetric{
 		Logql: &logql,
+	}
+}
+
+func marshalSLOCloudWatch(s *schema.Set) *n9api.CloudWatchMetric {
+	if s.Len() == 0 {
+		return nil
+	}
+
+	metric := s.List()[0].(map[string]interface{})
+
+	region := metric["region"].(string)
+
+	var namespace *string
+	if value := metric["namespace"].(string); value != "" {
+		namespace = &value
+	}
+
+	var metricName *string
+	if value := metric["metric_name"].(string); value != "" {
+		metricName = &value
+	}
+
+	var stat *string
+	if value := metric["stat"].(string); value != "" {
+		stat = &value
+	}
+
+	var sql *string
+	if value := metric["sql"].(string); value != "" {
+		sql = &value
+	}
+
+	var json *string
+	if value := metric["json"].(string); value != "" {
+		json = &value
+	}
+
+	var metricDimensions []n9api.CloudWatchMetricDimension
+
+	dimensions := metric["dimensions"].(*schema.Set)
+	for _, dimension := range dimensions.List() {
+		n9Dimension := dimension.(map[string]interface{})
+		name := n9Dimension["name"].(string)
+		value := n9Dimension["value"].(string)
+
+		metricDimensions = append(metricDimensions, n9api.CloudWatchMetricDimension{
+			Name:  &name,
+			Value: &value,
+		})
+	}
+
+	return &n9api.CloudWatchMetric{
+		Region:     &region,
+		Namespace:  namespace,
+		MetricName: metricName,
+		Stat:       stat,
+		Dimensions: metricDimensions,
+		SQL:        sql,
+		JSON:       json,
 	}
 }
 
@@ -859,6 +925,7 @@ func unmarshalSLOMetric(spec map[string]interface{}) (*schema.Set, error) {
 		{"opentsdb", "opentsdb", unmarshalOpentsdbMetric},
 		{"elasticsearch", "elasticsearch", unmarshalElasticsearchMetric},
 		{"grafana_loki", "grafanaLoki", unmarshalGrafanaLokiMetric},
+		{"cloudwatch", "cloudWatch", unmarshalCloudWatchMetric},
 	}
 
 	res := make(map[string]interface{})
@@ -973,6 +1040,19 @@ func unmarshalElasticsearchMetric(metric map[string]interface{}) map[string]inte
 func unmarshalGrafanaLokiMetric(metric map[string]interface{}) map[string]interface{} {
 	res := make(map[string]interface{})
 	res["logql"] = metric["logql"]
+
+	return res
+}
+
+func unmarshalCloudWatchMetric(metric map[string]interface{}) map[string]interface{} {
+	res := make(map[string]interface{})
+	res["region"] = metric["region"]
+	res["namespace"] = metric["namespace"]
+	res["metric_name"] = metric["metricName"]
+	res["stat"] = metric["stat"]
+	res["sql"] = metric["sql"]
+	res["json"] = metric["json"]
+	res["dimensions"] = metric["dimensions"]
 
 	return res
 }
