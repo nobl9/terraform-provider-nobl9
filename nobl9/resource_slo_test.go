5dcb340ad49df64a1206e247e8f77e1f961a8fe1
fpiwowarczyk <filip.piwowarczyk@nobl9.com>
2022-11-25 12:02:33 +0100

    add test conflicts

--- nobl9/resource_slo_test.go
+++ nobl9/resource_slo_test.go
@@ -99,82 +99,6 @@ func TestAcc_Nobl9SLOErrors(t *testing.T) {
 	}
 }
 
-func testPrometheusWithAttachmentsConflict(name string) string {
-	var serviceName = name + "-tf-service"
-	var agentName = name + "-tf-agent"
-	config :=
-		testService(serviceName) +
-			testPrometheusAgent(agentName) + `
-resource "nobl9_slo" ":name" {
-  name         = ":name"
-  display_name = ":name"
-  project      = ":project"
-  service      = nobl9_service.:serviceName.name
-
-  label {
-   key = "team"
-   values = ["green","sapphire"]
-  }
-
-  label {
-   key = "env"
-   values = ["dev", "staging", "prod"]
-  }
-
-  budgeting_method = "Occurrences"
-
-  objective {
-    display_name = "obj1"
-    name         = "tf-objective-1"
-    target       = 0.7
-    value        = 1
-    op           = "lt"
-    raw_metric {
-      query {
-        prometheus {
-          promql = "1.0"
-        }
-      }
-    }
-  }
-
-  time_window {
-    count      = 10
-    is_rolling = true
-    unit       = "Minute"
-  }
-
-  indicator {
-    name = nobl9_agent.:agentName.name
-    project = ":project"
-    kind    = "Agent"
-
-  }
-
-  attachment {
-    display_name = "test1"
-    url          = "https://google.com"
-  }
-
-  attachment {
-    display_name = "test1"
-    url          = "https://google.com"
-  }
-
-  attachments {
-    display_name = "test2"
-    url          = "https://google.com"
-  }
-}
-`
-	config = strings.ReplaceAll(config, ":name", name)
-	config = strings.ReplaceAll(config, ":serviceName", serviceName)
-	config = strings.ReplaceAll(config, ":agentName", agentName)
-	config = strings.ReplaceAll(config, ":project", testProject)
-
-	return config
-}
-
 func testAmazonPrometheusSLO(name string) string {
 	var serviceName = name + "-tf-service"
 	var agentName = name + "-tf-agent"
@@ -1804,6 +1728,82 @@ resource "nobl9_slo" ":name" {
 
   attachment {
     display_name = "test"
+    url          = "https://google.com"
+  }
+}
+`
+	config = strings.ReplaceAll(config, ":name", name)
+	config = strings.ReplaceAll(config, ":serviceName", serviceName)
+	config = strings.ReplaceAll(config, ":agentName", agentName)
+	config = strings.ReplaceAll(config, ":project", testProject)
+
+	return config
+}
+
+func testPrometheusWithAttachmentsConflict(name string) string {
+	var serviceName = name + "-tf-service"
+	var agentName = name + "-tf-agent"
+	config :=
+		testService(serviceName) +
+			testPrometheusAgent(agentName) + `
+resource "nobl9_slo" ":name" {
+  name         = ":name"
+  display_name = ":name"
+  project      = ":project"
+  service      = nobl9_service.:serviceName.name
+
+  label {
+   key = "team"
+   values = ["green","sapphire"]
+  }
+
+  label {
+   key = "env"
+   values = ["dev", "staging", "prod"]
+  }
+
+  budgeting_method = "Occurrences"
+
+  objective {
+    display_name = "obj1"
+    name         = "tf-objective-1"
+    target       = 0.7
+    value        = 1
+    op           = "lt"
+    raw_metric {
+      query {
+        prometheus {
+          promql = "1.0"
+        }
+      }
+    }
+  }
+
+  time_window {
+    count      = 10
+    is_rolling = true
+    unit       = "Minute"
+  }
+
+  indicator {
+    name = nobl9_agent.:agentName.name
+    project = ":project"
+    kind    = "Agent"
+
+  }
+
+  attachment {
+    display_name = "test1"
+    url          = "https://google.com"
+  }
+
+  attachment {
+    display_name = "test1"
+    url          = "https://google.com"
+  }
+
+  attachments {
+    display_name = "test2"
     url          = "https://google.com"
   }
 }
