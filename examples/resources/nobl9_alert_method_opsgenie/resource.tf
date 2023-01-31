a9d5dcdd236c0c11026321aac47c6477eedc41b5
tomasz.labuk <tomasz.labuk@nobl9.com>
2023-01-31 10:29:51 +0100

    fix opsgenie link 2

--- examples/resources/nobl9_alert_method_opsgenie/resource.tf
+++ examples/resources/nobl9_alert_method_opsgenie/resource.tf
@@ -3,7 +3,7 @@ resource "nobl9_alert_method_opsgenie" "this" {
   display_name = "My Opsgenie Alert"
   project      = "Test Project"
   description = "My Opsgenie Alert"
-  url         = "https://api.opsgenie.com.com"
+  url         = "https://api.opsgenie.com"
   auth		  = "GenieKey 12345"
 }
 
