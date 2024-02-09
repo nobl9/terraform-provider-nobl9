3b431175b791b9102291fa94523741a86beb4f20
krolik <szymon@nobl9.com>
2024-02-09 12:08:07 +0100

    Added group role binding example

--- examples/resources/nobl9_role_binding/resource.tf
+++ examples/resources/nobl9_role_binding/resource.tf
@@ -4,3 +4,10 @@ resource "nobl9_role_binding" "this" {
   role_ref    = "project-owner"
   project_ref = "1234567890asdfghjkl"
 }
+
+resource "nobl9_role_binding" "this" {
+  name        = "group-role-binding"
+  group_ref   = "group-name-12345abcde"
+  role_ref    = "project-owner"
+  project_ref = "1234567890asdfghjkl"
+}
