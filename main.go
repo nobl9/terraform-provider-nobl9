d8acbbdc112879befa832b4dab62af5d2641b212
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2024-02-13 11:04:49 +0100

    hardcode provider name

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
 //
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs -provider-name=terraform-provider-nobl9
 
 func main() {
 	var debugMode bool
