68d7e476b5689b7f5b2b9c59fc134fb1ea39e55c
bsski <swedrowskibartosz@gmail.com>
2024-02-28 11:04:26 +0100

    PC-12144 Honeycomb GA changes

--- nobl9/resource_agent.go
+++ nobl9/resource_agent.go
@@ -797,7 +797,6 @@ func marshalAgentGraphite(d *schema.ResourceData, diags diag.Diagnostics) *v1alp
 /**
  * Honeycomb Agent
  * https://docs.nobl9.com/Sources/honeycomb#honeycomb-agent
- * To access this integration, contact support@nobl9.com.
  */
 const honeycombAgentType = "honeycomb"
 const honeycombAgentConfigKey = "honeycomb_config"
