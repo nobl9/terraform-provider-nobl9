d6004374366d8133d7ef4e1f102dc16484bf0160
dawidwisn <76947218+dawidwisn@users.noreply.github.com>
2024-05-27 15:21:44 +0200

    Update examples/resources/nobl9_direct_logic_monitor/resource.tf
    Co-authored-by: paulina-n9 <65724378+paulina-n9@users.noreply.github.com>

--- examples/resources/nobl9_direct_logic_monitor/resource.tf
+++ examples/resources/nobl9_direct_logic_monitor/resource.tf
@@ -1,6 +1,6 @@
 resource "nobl9_direct_logic_monitor" "logic_monitor" {
   name = "logic-monitor"
-  project = "logic_monitor"
+  project = "logic-monitor"
   description = "desc"
   account = "account_name"
   account_id = "secret"
