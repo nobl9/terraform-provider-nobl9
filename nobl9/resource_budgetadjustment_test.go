cd7db68dee827425a85847d6007009586dee8aa2
Jakub Pięta <j.pieta96@gmail.com>
2024-07-10 09:43:30 +0200

    fix: PC-13500 dynamic date in BudgetAdjustment acceptance tests (#266)

--- nobl9/resource_budgetadjustment_test.go
+++ nobl9/resource_budgetadjustment_test.go
@@ -3,19 +3,22 @@ package nobl9
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/nobl9/nobl9-go/manifest"
 )
 
 func TestAcc_Nobl9BudgetAdjustments(t *testing.T) {
+	futureTime := time.Now().AddDate(0, 0, 1)
 	cases := []struct {
-		name       string
-		configFunc func(string) string
+		name            string
+		configFunc      func(string, time.Time) string
+		firstEventStart time.Time
 	}{
-		{"single-event", testBudgetAdjustmentSingleEvent},
-		{"recurring-event", testBudgetAdjustmentRecurringEvent},
-		{"recurring-event-multiple-slos", testBudgetAdjustmentRecurringEventMultipleSlo},
+		{"single-event", testBudgetAdjustmentSingleEvent, futureTime},
+		{"recurring-event", testBudgetAdjustmentRecurringEvent, futureTime},
+		{"recurring-event-multiple-slos", testBudgetAdjustmentRecurringEventMultipleSlo, futureTime},
 	}
 
 	for _, tc := range cases {
@@ -25,7 +28,7 @@ func TestAcc_Nobl9BudgetAdjustments(t *testing.T) {
 				CheckDestroy:      CheckDestroy("nobl9_budget_adjustment", manifest.KindBudgetAdjustment),
 				Steps: []resource.TestStep{
 					{
-						Config: tc.configFunc(tc.name),
+						Config: tc.configFunc(tc.name, tc.firstEventStart),
 						Check:  CheckObjectCreated(fmt.Sprintf("nobl9_budget_adjustment.%s", tc.name)),
 					},
 				},
@@ -34,13 +37,13 @@ func TestAcc_Nobl9BudgetAdjustments(t *testing.T) {
 	}
 }
 
-func testBudgetAdjustmentSingleEvent(name string) string {
+func testBudgetAdjustmentSingleEvent(name string, futureTime time.Time) string {
 	const sloName = "test-slo"
 	return testPrometheusSLOFull(sloName) +
 		fmt.Sprintf(`
 resource "nobl9_budget_adjustment" "%s" {
   name              = "%s"
-  first_event_start = "2022-01-01T00:00:00Z"
+  first_event_start = "%s"
   duration          = "1h"
   filters {
     slos {
@@ -51,10 +54,10 @@ resource "nobl9_budget_adjustment" "%s" {
     }
   }
 }
-`, name, name, sloName, testProject)
+`, name, name, futureTime.Format(time.RFC3339), sloName, testProject)
 }
 
-func testBudgetAdjustmentRecurringEvent(name string) string {
+func testBudgetAdjustmentRecurringEvent(name string, futureTime time.Time) string {
 	const sloName = "test-slo2"
 	const sloName2 = "test-slo3"
 
@@ -62,7 +65,7 @@ func testBudgetAdjustmentRecurringEvent(name string) string {
 		fmt.Sprintf(`
 resource "nobl9_budget_adjustment" "%s" {
   name              = "%s"
-  first_event_start = "2022-01-01T00:00:00Z"
+  first_event_start = "%s"
   duration          = "1h"
   rrule             = "FREQ=MONTHLY;BYMONTHDAY=1"
   filters {
@@ -77,10 +80,10 @@ resource "nobl9_budget_adjustment" "%s" {
       }
     }
   }
-}`, name, name, sloName, testProject, sloName2, testProject)
+}`, name, name, futureTime.Format(time.RFC3339), sloName, testProject, sloName2, testProject)
 }
 
-func testBudgetAdjustmentRecurringEventMultipleSlo(name string) string {
+func testBudgetAdjustmentRecurringEventMultipleSlo(name string, futureTime time.Time) string {
 	const sloName = "test-slo4"
 	const sloName2 = "test-slo5"
 
@@ -89,7 +92,7 @@ func testBudgetAdjustmentRecurringEventMultipleSlo(name string) string {
 resource "nobl9_budget_adjustment" "%s" {
   name              = "%s"
   display_name      = "Recurring budget adjustment for the first day of the month."
-  first_event_start = "2022-01-01T00:00:00Z"
+  first_event_start = "%s"
   description       = "Recurring budget adjustment for the first day of the month."
   duration          = "1h"
   rrule             = "FREQ=MONTHLY;BYMONTHDAY=1"
@@ -106,5 +109,5 @@ resource "nobl9_budget_adjustment" "%s" {
     }
   }
 }
-`, name, name, sloName, testProject, sloName2, testProject)
+`, name, name, futureTime.Format(time.RFC3339), sloName, testProject, sloName2, testProject)
 }
