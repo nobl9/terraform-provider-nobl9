6c37840297eb6d0c6d2aa2a06a6f6346720bff18
Marcin Ławniczak <marcin.lawniczak@nobl9.com>
2024-11-14 12:44:49 +0100

    test: Add test for LogicMonitor historical_data_retrieval (#303)
    ## Motivation
    
    https://github.com/nobl9/terraform-provider-nobl9/pull/292 introduced
    Logic Monitor Historical Data Retrieval, but it was not reflected in
    tests.
    Test was added for GCM direct.
    
    ## Summary
    
    Added test
    
    ## Related Changes
    
    https://github.com/nobl9/terraform-provider-nobl9/pull/292
    
    ## Testing
    
    - No manual testing needed, automated test coverage extended.

--- nobl9/resource_direct_test.go
+++ nobl9/resource_direct_test.go
@@ -407,6 +407,16 @@ resource "nobl9_direct_%s" "%s" {
   account = "account_name"
   account_id = "secret"
   access_key = "secret"
+  historical_data_retrieval {
+    default_duration  {
+      unit = "Day"
+      value = 1
+    }
+    max_duration {
+      unit = "Day"
+      value = 10
+    }
+  }
   log_collection_enabled = true
   release_channel = "beta"
   query_delay {
