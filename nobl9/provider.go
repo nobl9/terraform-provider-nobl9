2dce96637bfc6849a18017a473ddb3a2b0cb8682
daniel.zelazny <daniel.zelazny@nobl9.com>
2025-03-18 21:44:12 +0100

    Non prefixed env variable for old provider

--- nobl9/provider.go
+++ nobl9/provider.go
@@ -164,10 +164,8 @@ func getClient(providerConfig ProviderConfig) (*sdk.Client, diag.Diagnostics) {
 			sdk.ConfigOptionEnvPrefix("TERRAFORM_NOBL9_"),
 		}
 		if providerConfig.NoConfigFile {
-			println("No config file option")
 			options = append(options, sdk.ConfigOptionNoConfigFile())
 		}
-		println("-- further step -- ")
 
 		sdkConfig, err := sdk.ReadConfig(options...)
 		if err != nil {
