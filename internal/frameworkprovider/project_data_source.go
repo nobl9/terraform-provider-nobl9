4a29b1781f3c66a2f0b32e1ac4bb7a05eb8fd72d
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2025-04-11 18:18:43 +0200

    update docs

--- internal/frameworkprovider/project_data_source.go
+++ internal/frameworkprovider/project_data_source.go
@@ -23,7 +23,11 @@ type ProjectDataSource struct {
 }
 
 // Metadata returns the data source type name.
-func (d *ProjectDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+func (d *ProjectDataSource) Metadata(
+	_ context.Context,
+	req datasource.MetadataRequest,
+	resp *datasource.MetadataResponse,
+) {
 	resp.TypeName = req.ProviderTypeName + "_project"
 }
 
