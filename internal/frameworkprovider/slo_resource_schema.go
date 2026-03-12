ee0ffd92b1cd1fd682637099ca610c8d06f8b342
Mateusz Karasiewicz <mateusz.karasiewicz@nobl9.com>
2026-03-12 18:41:47 +0100

    feat: add AccountGroupID support to ThousandEyes SLO metric
    Add optional account_group_id field to the ThousandEyes metric block in
    SLO resources. This aligns the provider with nobl9-go v0.125.0 which
    introduced AccountGroupID on ThousandEyesMetric.
    
    Also bumps nobl9-go from v0.122.2 to v0.125.0 and updates transitive
    dependencies (govy, golang-jwt, golang.org/x/{net,tools}).

--- internal/frameworkprovider/slo_resource_schema.go
+++ internal/frameworkprovider/slo_resource_schema.go
@@ -929,6 +929,10 @@ func sloResourceMetricSpecBlocks() map[string]schema.Block {
 						Optional:    true,
 						Description: "Type of the test",
 					},
+					"account_group_id": schema.Int64Attribute{
+						Optional:    true,
+						Description: "ID of the account group",
+					},
 				},
 			},
 		},
