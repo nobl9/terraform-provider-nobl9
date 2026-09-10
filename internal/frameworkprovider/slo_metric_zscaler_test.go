3e712ca88ddc97d08f3661299e525fcd9ab09a46
Mateusz Karasiewicz <mateusz.karasiewicz@nobl9.com>
2026-09-10 12:28:58 +0200

    feat: configure Zscaler report types in SLO queries

--- internal/frameworkprovider/slo_metric_zscaler_test.go
+++ internal/frameworkprovider/slo_metric_zscaler_test.go
@@ -13,7 +13,7 @@ import (
 
 func TestZscalerMetricRoundTrip(t *testing.T) {
 	t.Parallel()
-	for _, metric := range []string{"score", "pft", "dns", "availability"} {
+	for _, metric := range []string{"score", "pft"} {
 		for _, withLocation := range []bool{false, true} {
 			name := metric + "/all-locations"
 			if withLocation {
@@ -21,7 +21,7 @@ func TestZscalerMetricRoundTrip(t *testing.T) {
 			}
 			t.Run(name, func(t *testing.T) {
 				t.Parallel()
-				src := &v1alphaSLO.ZscalerMetric{AppID: 9007199254740993, Metric: metric}
+				src := &v1alphaSLO.ZscalerMetric{Type: "application", AppID: 9007199254740993, Metric: metric}
 				if withLocation {
 					locationID := int64(123)
 					src.LocationID = &locationID
@@ -41,6 +41,7 @@ func TestZscalerMetricOmittedAndExplicitZeroLocation(t *testing.T) {
 	assert.Nil(t, modelToZscaler(nil))
 	assert.Nil(t, (MetricSpecModel{}).ToManifest().Zscaler)
 	model := ZscalerModel{
+		Type:  types.StringValue("application"),
 		AppID: types.Int64Value(123), Metric: types.StringValue("score"), LocationID: types.Int64Value(0),
 	}
 	actual := modelToZscaler(&model)
@@ -54,6 +55,8 @@ func TestZscalerMetricUnknownPlanValues(t *testing.T) {
 		Zscaler []ZscalerModel `tfsdk:"zscaler"`
 	}
 	model := query{Zscaler: []ZscalerModel{{
+		Type: types.StringUnknown(), DeviceID: types.Int64Unknown(), ProbeID: types.Int64Unknown(),
+		LegSrc: types.StringUnknown(), LegDst: types.StringUnknown(),
 		AppID: types.Int64Unknown(), LocationID: types.Int64Unknown(), Metric: types.StringUnknown(),
 	}}}
 	plan := tfsdk.Plan{Schema: schema.Schema{
@@ -63,4 +66,23 @@ func TestZscalerMetricUnknownPlanValues(t *testing.T) {
 	var actual query
 	require.False(t, plan.Get(t.Context(), &actual).HasError())
 	assert.Equal(t, model, actual)
+}
+
+func TestZscalerProbeMetricRoundTrip(t *testing.T) {
+	deviceID, probeID := int64(67890), int64(24680)
+	src, dst := "client", "egress"
+	for _, query := range []v1alphaSLO.ZscalerMetric{
+		{Type: "web-probe", Metric: "ttfb", AppID: 12345, DeviceID: &deviceID, ProbeID: &probeID},
+		{Type: "cloudpath", Metric: "loss", AppID: 12345, DeviceID: &deviceID, ProbeID: &probeID},
+		{Type: "cloudpath", Metric: "latency", AppID: 12345,
+			DeviceID: &deviceID, ProbeID: &probeID, LegSrc: &src, LegDst: &dst},
+	} {
+		t.Run(query.Type+"/"+query.Metric, func(t *testing.T) {
+			model := zscalerToModel(&query)
+			assert.True(t, model.LocationID.IsNull())
+			assert.Equal(t, query.LegSrc == nil, model.LegSrc.IsNull())
+			assert.Equal(t, query.LegDst == nil, model.LegDst.IsNull())
+			assert.Equal(t, &query, modelToZscaler(model))
+		})
+	}
 }
